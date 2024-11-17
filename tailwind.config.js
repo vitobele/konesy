90672ac2868a0477adc17a3e3ad28ac20a5be739
vitobele <vitobeleblog@gmail.com>
2024-11-17 07:31:29 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {module.exports = {
+    content: [
+      "./src/**/**/*.{html,js,css}", // Tambahkan ekstensi .css
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
