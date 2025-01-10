0d536f617d1f7b4ce814f81be64eb67c42548d49
vitobele <vitobeleblog@gmail.com>
2025-01-10 08:38:56 +0700

    add data site.js

---
+++ src/_data/site.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: "Konesy",
+    description:
+      "Kami mengumpulkan informasi maupun resource digital, mengolahnya, memvalidasi, dan mengelompokkan ke berbagai fitur untuk anda konsumsi serta anda gunakan kembali.",
+    language: "id",
+    url: "https://konesy.com",
+    author: "konesy",
+    social: {
+      twitter: "@yourtwitterhandle",
+      github: "https://github.com/yourusername",
+    },
+    img:"/assets/img/", // shortcut link
+  };
+  
