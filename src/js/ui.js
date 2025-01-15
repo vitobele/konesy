0c9e2f895417aa5a22f15ee5ecd7ec5cd09a459e
vitobele <vitobeleblog@gmail.com>
2025-01-15 19:35:54 +0700

    uncomment ui.js script console

--- src/js/ui.js
+++ src/js/ui.js
@@ -15,8 +15,8 @@ document.addEventListener("DOMContentLoaded", function () {
             // Menghitung nomor urut dengan memperhitungkan atribut "start"
             const itemNumber = startValue + index;
             const numDigits = itemNumber.toString().length;
-            // Panjang digit dari nomor urut Log ke konsol untuk debugging
-            console.log('Item:', itemNumber, 'Panjang Digit:', numDigits);
+            // Panjang digit dari nomor urut Log ke konsol untuk debugging (uncomment script console untuk debuging)
+            // console.log('Item:', itemNumber, 'Panjang Digit:', numDigits);
             // Jika nomor urut lebih dari 2 digit, berikan margin sesuai dengan panjang digit
             if (numDigits > 2) { // Menyesuaikan kondisi untuk lebih dari 2 digit
                 const marginLeft = numDigits * 0.5 + 'rem'; // Margin dinamis berdasarkan panjang digit
